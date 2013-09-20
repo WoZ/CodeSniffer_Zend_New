379f18448426422b0cf2480970d49f82ea2feb96
Dmitry Menshikov <d.menshikov@gmail.com>
2013-09-20 17:24:24 +0300

    No file comments

--- Sniffs/Commenting/ClassCommentSniff.php
+++ Sniffs/Commenting/ClassCommentSniff.php
@@ -20,11 +20,6 @@ if (class_exists('PHP_CodeSniffer_CommentParser_ClassCommentParser', true) === f
     throw new PHP_CodeSniffer_Exception($error);
 }
 
-if (class_exists('ZendNew_Sniffs_Commenting_FileCommentSniff', true) === false) {
-    $error = 'Class ZendNew_Sniffs_Commenting_FileCommentSniff not found';
-    throw new PHP_CodeSniffer_Exception($error);
-}
-
 /**
  * Parses and verifies the doc comments for classes.
  * That's basically the same as the FileCommentSniff. So just register the
@@ -41,24 +36,119 @@ if (class_exists('ZendNew_Sniffs_Commenting_FileCommentSniff', true) === false) 
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  * @see       ZendNew_Sniffs_Commenting_FileCommentSniff
  */
-class ZendNew_Sniffs_Commenting_ClassCommentSniff extends ZendNew_Sniffs_Commenting_FileCommentSniff
+class ZendNew_Sniffs_Commenting_ClassCommentSniff extends PEAR_Sniffs_Commenting_ClassCommentSniff implements PHP_CodeSniffer_Sniff
 {
-
+    /**
+     * Tags in correct order and related info.
+     *
+     * @var array
+     */
+    protected $tags = array(
+                       'category'   => array(
+                                        'required'       => false,
+                                        'allow_multiple' => false,
+                                        'order_text'     => 'precedes @package',
+                                       ),
+                       'package'    => array(
+                                        'required'       => true,
+                                        'allow_multiple' => false,
+                                        'order_text'     => 'follows @category',
+                                       ),
+                       'subpackage' => array(
+                                        'required'       => false,
+                                        'allow_multiple' => false,
+                                        'order_text'     => 'follows @package',
+                                       ),
+                       'author'     => array(
+                                        'required'       => false,
+                                        'allow_multiple' => true,
+                                        'order_text'     => 'follows @subpackage (if used) or @package',
+                                       ),
+                       'copyright'  => array(
+                                        'required'       => false,
+                                        'allow_multiple' => true,
+                                        'order_text'     => 'follows @author',
+                                       ),
+                       'license'    => array(
+                                        'required'       => false,
+                                        'allow_multiple' => false,
+                                        'order_text'     => 'follows @copyright (if used) or @author',
+                                       ),
+                       'version'    => array(
+                                        'required'       => false,
+                                        'allow_multiple' => false,
+                                        'order_text'     => 'follows @license',
+                                       ),
+                       'link'       => array(
+                                        'required'       => false,
+                                        'allow_multiple' => true,
+                                        'order_text'     => 'follows @version',
+                                       ),
+                       'see'        => array(
+                                        'required'       => false,
+                                        'allow_multiple' => true,
+                                        'order_text'     => 'follows @link',
+                                       ),
+                       'since'      => array(
+                                        'required'       => false,
+                                        'allow_multiple' => false,
+                                        'order_text'     => 'follows @see (if used) or @link',
+                                       ),
+                       'deprecated' => array(
+                                        'required'       => false,
+                                        'allow_multiple' => false,
+                                        'order_text'     => 'follows @since (if used) or @see (if used) or @link',
+                                       ),
+                );
 
     /**
-     * Returns an array of tokens this test wants to listen for.
+     * Process the version tag.
      *
-     * @return array
+     * @param int $errorPos The line number where the error occurs.
+     *
+     * @return void
      */
-    public function register()
+    protected function processVersion($errorPos)
     {
-        return array(
-                T_CLASS,
-                T_INTERFACE,
-               );
+        $version = $this->commentParser->getVersion();
+        if ($version !== null) {
+            $content = $version->getContent();
+            $matches = array();
+            if (empty($content) === true) {
+                $error = 'Content missing for @version tag in file comment';
+                $this->currentFile->addError($error, $errorPos, 'EmptyVersion');
+            }
+        }
 
-    }//end register()
+    }//end processVersion()
 
+    /**
+     * Process the copyright tags.
+     *
+     * @param int $commentStart The position in the stack where
+     *                          the comment started.
+     *
+     * @return void
+     */
+    protected function processCopyrights($commentStart)
+    {
+        $copyrights = $this->commentParser->getCopyrights();
+
+        foreach ($copyrights as $copyright) {
+
+            $errorPos = ($commentStart + $copyright->getLine());
+
+            $content = $copyright->getContent();
+
+            if (empty($content)) {
+
+                $this->currentFile->addError('A copyright-tag must have content', $errorPos, 'CopyrightNoContent');
+
+            }
+
+        }
+
+    }
 
 }//end class
 
